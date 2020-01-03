f8bbf992130b97436f8c22fbba71addc6e140da0
unknown <nforesperance1@gmail.com>
2020-01-03 11:03:27 +0100

    Third Commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 // Database connection
-const sequelize = new Sequelize('controle', 'root', '', {
+const sequelize = new Sequelize('database', 'username', 'password', {
     host: 'localhost',
     dialect: 'mysql'
   });
